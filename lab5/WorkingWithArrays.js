8b8d91a0e3dc169d5dbf8b469f343fba8f8dd817
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 17:50:34 -0400

    5.2.6.4

--- lab5/WorkingWithArrays.js
+++ lab5/WorkingWithArrays.js
@@ -62,11 +62,20 @@ export default function WorkingWithArrays(app) {
   const deleteTodo = (req, res) => {
     const { id } = req.params;
     const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
     todos.splice(todoIndex, 1);
     res.sendStatus(200);
   };
   const updateTodo = (req, res) => {
     const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
     todos = todos.map((t) => {
       if (t.id === parseInt(id)) {
         return { ...t, ...req.body };
