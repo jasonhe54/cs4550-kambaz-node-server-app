8dff8b288becf776892fbd1952d5dca13cabedeb
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 18:42:55 -0400

    5.4.2.5

--- kambaz/users/routes.js
+++ kambaz/users/routes.js
@@ -6,7 +6,13 @@ export default function UserRoutes(app, db) {
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
-  const updateUser = (req, res) => { };
+  const updateUser = (req, res) => {
+    const userId = req.params.userId;
+    const userUpdates = req.body;
+    dao.updateUser(userId, userUpdates);
+    currentUser = dao.findUserById(userId);
+    res.json(currentUser);
+  };
   const signup = (req, res) => {
     const user = dao.findUserByUsername(req.body.username);
     if (user) {
@@ -21,8 +27,13 @@ export default function UserRoutes(app, db) {
     currentUser = dao.findUserByCredentials(username, password);
     res.json(currentUser);
   };
-  const signout = (req, res) => { };
-  const profile = (req, res) => { };
+  const signout = (req, res) => {
+    currentUser = null;
+    res.sendStatus(200);
+  };
+  const profile = (req, res) => {
+    res.json(currentUser);
+  };
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
