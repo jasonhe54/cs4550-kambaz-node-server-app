ca5797e801066d4ede5d9c4cf61c14f2ee10c07e
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 18:31:59 -0400

    5.4.2.2

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import hello from "./hello.js"
 import lab5 from "./lab5/index.js";
 import cors from "cors";
+import db from "./kambaz/database/index.js";
+import UserRoutes from "./kambaz/users/routes.js";
 
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app, db);
 lab5(app)
 hello(app)
 app.listen(process.env.PORT || 4000)
