34dc9bcd34816d80b2d9d85da81bf216b99db75d
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-03-28 16:00:12 -0400

    5.2.2.3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import hello from './hello.js'
-import lab5 from './lab5/index.js'
+import hello from "./hello.js"
+import lab5 from "./lab5/index.js";
 
 const app = express()
 lab5(app)
 hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
