c33d01ab1e5eb4729810e5df0e5d4ceb93e8ff69
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2026-04-10 11:36:56 -0400

    6.4.2.1

--- kambaz/courses/dao.js
+++ kambaz/courses/dao.js
@@ -3,7 +3,7 @@ import model from "./model.js";
 
 export default function CoursesDao(db) {
   function findAllCourses() {
-    return model.find();
+    return model.find({}, { name: 1, description: 1 });
   }
 
   function findCourseById(courseId) {
@@ -12,7 +12,7 @@ export default function CoursesDao(db) {
 
   async function findCoursesForEnrolledUser(userId) {
     const { enrollments } = db;
-    const courses = await model.find();
+    const courses = await model.find({}, { name: 1, description: 1 });
     const enrolledCourses = courses.filter((course) =>
       enrollments.some(
         (enrollment) => enrollment.user === userId && enrollment.course === course._id
@@ -32,7 +32,7 @@ export default function CoursesDao(db) {
   }
 
   function updateCourse(courseId, courseUpdates) {
-    return model.updateOne({ _id: courseId}, { $set: courseUpdates });
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
   }
 
   return {
